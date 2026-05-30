5bf3dbcdd2f60bdff8341f8d8ae88296284e54f1
harlock024 <hadith024@gmail.com>
2026-05-30 11:19:28 -0600

    perf(branches): use CancellableNetworkTileProvider for flutter_map on web
    Eliminates the flutter_map performance warning on web by using the
    official cancellable tile provider that cancels in-flight HTTP requests
    when tiles are no longer needed.

--- lib/features/branches/branches_screen.dart
+++ lib/features/branches/branches_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -326,6 +327,7 @@ class _MiniMap extends StatelessWidget {
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.omnigym.app',
+          tileProvider: CancellableNetworkTileProvider(),
         ),
         MarkerLayer(
           markers: [
