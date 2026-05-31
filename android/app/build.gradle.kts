1c3a2c1e46f98aa8f5704e310230ce308cbbb1be
harlock024 <hadith024@gmail.com>
2026-05-30 23:45:43 -0600

    fix(android): remove ndkVersion to fix build when NDK not installed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
 android {
     namespace = "com.example.omnigym"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
